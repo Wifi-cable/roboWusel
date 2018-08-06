67a20ee8a9627ba035629e8a41fbf4686ce30b5f
wifi-cable <Wifi-cable@users.noreply.github.com>
2018-08-06 11:47:06 +0200

    software PWM for LEDs in ISR

--- wusel.c
+++ wusel.c
@@ -12,16 +12,17 @@
 #define leftFrontLeg	OCR1B
 #define rightBackLeg 	OCR1C
 #define leftBackLeg   OCR3A
+int mode;
 
-
-void blink();
+void timerInit();
 void blinkTimes(int time);
 void setup();
 void hello(int times);
 void spikesSetup();
 void wakeUp();
-void openEyes();
+
 void getUp();
+
 
 void main(void){
 
@@ -38,40 +39,17 @@ void main(void){
 	int gyro; 
 	
 	sei();
-	blink();
+	timerInit();
  	spikesSetup();
- 
+ 	_delay_ms(250);
+ 	mode=1;
+
 	while(1){
 
 	}
 
 }
-void setup(){
-	// set up input, output pins 
-	
-/*DDB7 = backRightServo
-	DDB6 = frontLeftServo 
-	DDB5 = frontRightServo 
-	DDB4 = distanceSensorOn/off */
-	DDRB = (1<<DDB7) | 	(1<<DDB6) |	 (1<<DDB5) | 	(1<< DDB4); // servo motor output enabled
-	//DDC6 = backLeftServo
-	//DDC7 = LEDright
-	DDRC = (1<<DDC7) | (1<<DDC6); 
-	//DDD5 = LEDleft
-	DDRD = (1<<DDD5);	
-	
-	//servo setup, fast PWM or mode 14 for all hardware PWM pins
 
-	TCCR1A = (1<< COM1A1)| (1<< WGM11); 
-	TCCR1B = (1<< WGM12) | (1<<WGM13) ;
-	
-	TCCR3A = (1<< WGM31) | (1<<COM3A1);
-	TCCR3B = (1<< WGM32) | (1<<WGM33) ;
-	
-
-	ICR1 = 19999;
-
-}
 
 void spikesSetup(){
 			DDRB = (1<<DDB7) | 	(1<<DDB6) |	 (1<<DDB5) | 	(1<< DDB4); // servo motor output enabled
@@ -87,11 +65,11 @@ void spikesSetup(){
         TCCR1B = ((0<<ICNC1) | (0<<ICES1) | (0<<PIN5) | (1<<WGM13) | (1<<WGM12)| (0<<CS12) | (1<<CS11) | (0<<CS10));
         
         //OCR1A  = 2000;     // SERVO_VR
-        rightFrontLeg = 2000;
+        rightFrontLeg = 4000;
        // OCR1B  = 2000;     // SERVO_VL
         leftFrontLeg =2000;
        // OCR1C  = 2000;     // SERVO_HR
-        rightBackLeg =2000;
+        rightBackLeg =4000;
         
         ICR1   = 40000; // Periode 	
         
@@ -104,17 +82,19 @@ void spikesSetup(){
         ICR3   = 40000; // Periode 	
 	}
 
-void blink(){
+void timerInit(){
 /* blink init is timer init*/
-	TCCR0B = (1<<CS02) | (1<<CS00);	//prescaler 1024
+	TCCR0B = (1<<CS01) | (1<<CS00) | (1<<WGM02);	//prescaler 64
 	TIMSK0 |= (1<<TOIE0);	//timer counter over flow is enabled
+	TCCR0A |= (1<<WGM01)| (1<<WGM00);
+	OCR0A = 249; 
 	TCNT0 = 0;	//timer starts at zero
 	PORTD |= (1 << leftEye);
 
 }
 	void hello(int times){
-		int counter=0;
-		while(counter<times){
+		int counter = 0;
+		while(counter < times){
 			OCR1A = 2000; 
 			_delay_ms(250);
 			OCR1A = 2000+ 1000; 
@@ -139,49 +119,16 @@ void blink(){
 	set back servos to push the rear up.*/
 	void wakeUp(){	
 	
-	openEyes();
-	blink(4);
+
+	blinkTimes(4);
 	getUp();
 	
 	
 	}
 	
-	void openEyes(){
-	uint8_t counter = 0;
-	//pwm implementation in order to understand PWM myself
-	int idx = 0;
-		
-		uint8_t high = 0;
-		uint8_t low = 60;
-		int hiIdx = 0; 
-		int loIdx = 0;
-		
-	while (idx < 20){
-	
-		hiIdx = 0; 
-	  loIdx = 0;
-		PORTC |= (1<< rightEye);
-		PORTD |= (1<<leftEye); 
-		
-		while(hiIdx <= high){//how long on?
-			hiIdx++;
-		}
-		PORTC |= (0<< rightEye);
-		PORTD |= (0<<leftEye); 
-			
-		while(loIdx <= low){// how long off
-			loIdx++;
-		}
-		
-		if(counter%20==0){
-			idx++;
-			
-			high =+3;
-			low =-3;
-		}
-		counter++;
-	}
-		}
+
+
+
 		void getUp(){
 		// slowly set front legs to angle ?
 		int counter=0;
@@ -197,11 +144,46 @@ void blink(){
 		/*interrupt service routine that makes the eyes blink twice per second. 
 		(16 000 000 /1024)/ 256 = 60 hz. 60*30 so they togle every half second */
 	ISR(TIMER0_OVF_vect) {
-	static uint8_t overflow;
-	overflow++;
-	if(overflow>=30){
+	static uint8_t ms250;
+	static uint8_t ms500;
+	uint8_t ms10;
+	static uint8_t on = 0; 
+	static int pwmCount = 20; 
+	static int brightnes = 0;
+	
+	
+	ms250++;
+	if((ms250 % 100)== 0 ){
+		ms10++;
+		
+		if(on<20){
+			on++;
+		}
+	}
+	
+	if(ms250 >=250){
+		ms500++;
+		ms250=0; 
+		
+	}
+	
+	if((ms500 >= 2)&& (mode==1)){// every half second 
 		PORTC ^= (1 << rightEye);
 		PORTD ^= (1 << leftEye);
-		overflow = 0;
+		ms500= 0;
+		}
+		
+  if(brightnes<=on){// start with 0
+			PORTC &= ~ (1<< rightEye);
+			PORTD &= ~(1<<leftEye); 
+		}
+	else{//start with 20
+			PORTC |= (1<< rightEye);
+			PORTD |= (1<<leftEye); 
+		
+		}
+		brightnes++; 
+		if(brightnes>20){
+			brightnes=0;
 		}
 	}
